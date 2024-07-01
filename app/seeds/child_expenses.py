425d7f6205e871f470a5414152490830e38a8a6d
David Liu <davidas99017@gmail.com>
2024-07-01 16:57:01 -0400

    added Phillip's seeders to the team2 branch

--- app/seeds/child_expenses.py
+++ app/seeds/child_expenses.py
@@ -1,36 +1,40 @@
 from app.models import db, ChildExpense, environment, SCHEMA
-from sqlalchemy.sql import text 
+from sqlalchemy.sql import text
+from alembic import op
 
-# Adds a demo user, you can add other users here if you want
-def seed_child_expense():
-    expense1 = ChildExpense(
-        root_expense_id = 1, 
-        user_id = 2, 
-        split_amount = 50, 
+def seed_child_expenses():
+
+    dinner_expense1 = ChildExpense(
+        root_expense_id = 1, user_id=2,split_amount=83.34
     )
-    expense2 = ChildExpense(
-        root_expense_id = 1, 
-        user_id = 3, 
-        split_amount = 50, 
+
+    dinner_expense2 = ChildExpense(
+        root_expense_id = 1, user_id=3,split_amount=83.34
     )
-    expenses = [expense1, expense2]
-    for exp in expenses: 
-        db.session.add(exp)
+
+    grocery_expense = ChildExpense(
+        root_expense_id = 2, user_id=2,split_amount=40
+    )
+
+    travel_expense1 = ChildExpense(
+        root_expense_id = 3, user_id=1,split_amount=266.67
+    )
+
+    travel_expense2 = ChildExpense(
+        root_expense_id = 3, user_id=3,split_amount=266.67
+    )
+
+    db.session.add(dinner_expense1)
+    db.session.add(dinner_expense2)
+    db.session.add(grocery_expense)
+    db.session.add(travel_expense1)
+    db.session.add(travel_expense2)
     db.session.commit()
-    # db.session.add(bobbie)
-    # db.session.commit()
 
-# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# have a built in function to do this. With postgres in production TRUNCATE
-# removes all the data from the table, and RESET IDENTITY resets the auto
-# incrementing primary key, CASCADE deletes any dependent entities.  With
-# sqlite3 in development you need to instead use DELETE to remove all data and
-# it will reset the primary keys for you as well.
-def undo_child_expense():
+def undo_child_expenses():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.child_expenses RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM child_expenses"))
-        
-    db.session.commit()
 
+    db.session.commit()
--- app/seeds/root_expenses.py
+++ app/seeds/root_expenses.py
@@ -1,43 +1,30 @@
 from app.models import db, RootExpense, environment, SCHEMA
-from sqlalchemy.sql import text 
+from sqlalchemy.sql import text
+from alembic import op
 
-# Adds a demo user, you can add other users here if you want
-def seed_root_expense():
-    expense1 = RootExpense(
-        owner_id = 1, 
-        name = "Lunch", 
-        amount = 100, 
-        expense_type = "Food", 
+def seed_root_expenses():
+
+    small_expense = RootExpense(
+        owner_id=1, name="Dinner", amount="250", expense_type="Food"
     )
-    expense2 = RootExpense(
-        owner_id = 2,
-        name = 'Dinner', 
-        amount = 200, 
-        expense_type = "Food", 
+
+    smaller_expense = RootExpense(
+        owner_id=1, name="Groceries", amount="80", expense_type="Groceries"
     )
-    expense3 = RootExpense(
-        owner_id = 3, 
-        name = "Gas", 
-        amount = 200, 
-        expense_type = "Travel", 
+
+    big_expense = RootExpense(
+        owner_id=2, name="AirBnb", amount="800", expense_type="Travel"
     )
-    expenses = [expense1, expense2, expense3]
-    for exp in expenses: 
-        db.session.add(exp)
+
+    db.session.add(small_expense)
+    db.session.add(smaller_expense)
+    db.session.add(big_expense)
     db.session.commit()
-    # db.session.add(bobbie)
-    # db.session.commit()
 
-# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# have a built in function to do this. With postgres in production TRUNCATE
-# removes all the data from the table, and RESET IDENTITY resets the auto
-# incrementing primary key, CASCADE deletes any dependent entities.  With
-# sqlite3 in development you need to instead use DELETE to remove all data and
-# it will reset the primary keys for you as well.
-def undo_root_expense():
+def undo_root_expenses():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.root_expenses RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM root_expenses"))
-        
+
     db.session.commit()
