f8b50c8e680d4165810e5ad7792d2ecda6bdbbcc
David Liu <davidas99017@gmail.com>
2024-07-01 11:30:12 -0400

    added get single expense details route

--- app/api/root_expense_routes.py
+++ app/api/root_expense_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from .utils import ExpenseUtils
+from .utils import ExpenseUtils, AuthUtils
 from flask_login import login_required
 
 root = Blueprint("root_expenses", __name__)
@@ -14,7 +14,16 @@ def post_new_expense():
     req_body = request.get_json()
     return ExpenseUtils.create_new_expense(req_body)
 
-@root.route("/<int: id>", methods = ["PUT"])
+@root.route("/<int:id>", methods = ["GET"])
+def get_expense(id): 
+    expense = ExpenseUtils.get_expense_by_id(int(id))
+    if expense.owner_id == AuthUtils.get_current_user()['id']: 
+        return ExpenseUtils.parse_data(expense)
+    else: 
+        return {"message": "Not Authorized"}
+
+
+@root.route("/<int:id>", methods = ["PUT"])
 def update_expense():
     req_body = request.get_json()
     return ExpenseUtils.update_expense_by_id(id, req_body)
---
+++ app/seeds/child_expenses.py
@@ -0,0 +1,26 @@
+from app.models import db, ChildExpense, environment, SCHEMA
+from sqlalchemy.sql import text 
+
+# Adds a demo user, you can add other users here if you want
+def seed_child_expense():
+    expenses = []
+    for exp in expenses: 
+        db.session.add(exp)
+    db.session.commit()
+    # db.session.add(bobbie)
+    # db.session.commit()
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_child_expense():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.child_expenses RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM child_expenses"))
+        
+    db.session.commit()
+
