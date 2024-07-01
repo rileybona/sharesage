a6d77b9f6e9bf9bdaa26bddb3c309888d56044af
David Liu <davidas99017@gmail.com>
2024-07-01 17:02:00 -0400

    fixed seeder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,31 +1,40 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
-from .root_expenses import seed_root_expense, undo_root_expense
-from .child_expenses import seed_child_expense, undo_child_expense
-from app.models.db import environment
-from app.models import db
+from .root_expenses import seed_root_expenses, undo_root_expenses
+from .child_expenses import seed_child_expenses, undo_child_expenses
+from .comments import seed_comments, undo_comments
+from .payments import seed_payments, undo_payments
 
-# `flask seed help`
+from app.models.db import db, environment, SCHEMA
+
+# Creates a seed group to hold our commands
+# So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 
-# flask seed all
-# might want to RUN `flask seed undo` before seeding
+# Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
+        # the schema name (see comment in users.py undo_users function).
+        # Make sure to add all your other model's undo functions below
         undo_users()
-    else:
-        db.drop_all() 
-        db.create_all()
     seed_users()
-    seed_root_expense()
-    seed_child_expense()
+    # Add other seed functions here
+    seed_root_expenses()
+    seed_child_expenses()
+    seed_comments()
+    seed_payments()
 
-# flask seed undo
+
+# Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    undo_root_expense()
-    undo_child_expense()
-    
+    # Add other undo functions here
+    undo_root_expenses()
+    undo_child_expenses()
+    undo_comments()
+    undo_payments()
