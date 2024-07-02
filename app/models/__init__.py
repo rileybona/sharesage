8454c2293fcbcbcbe00e032108bc0241758fbb9b
David Liu <davidas99017@gmail.com>
2024-07-02 18:02:28 -0400

    removing stuff

--- app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .db import db
-from .user import User
-from .child_expense import ChildExpense
-from .comment import Comment
-from .payment import Payment
-from .root_expense import RootExpense
-from .db import environment, SCHEMA
