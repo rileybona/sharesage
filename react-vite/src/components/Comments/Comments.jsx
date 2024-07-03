ed9006049d45b8d9216e5bc5dfbd7d5d586df1ca
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-03 18:07:24 -0500

    Formatted Comment Templates

--- react-vite/src/components/Comments/Comments.jsx
+++ react-vite/src/components/Comments/Comments.jsx
@@ -22,9 +22,15 @@ function Comments() {
     <div id="comments">
       <h1>Comments</h1>
       <button>Post your comment</button>
-      {/* {userComments?.map((comment, i) => (
-        <div key={i}>{JSON.stringify(comment)}</div>
-      ))} */}
+      {userComments?.map((comment, i) => (
+        <div className="comment" key={i}>
+          <div className="comment-header">date by FirstName</div>
+          <div className="comment-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, cumque! Molestiae natus excepturi explicabo labore assumenda, expedita animi minima aliquam voluptatem aperiam neque qui omnis nihil obcaecati ullam provident odio.</div>
+          <div className="comment-buttons">
+            <button>Update</button><button>Delete</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
