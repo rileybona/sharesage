6bf1898bf48681918821fecfb9986e116db0213f
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-03 12:58:50 -0500

    Refactored Thunks

--- react-vite/src/redux/comment.js
+++ react-vite/src/redux/comment.js
@@ -3,45 +3,47 @@ const POST_COMMENT = "comment/postComment";
 const UPDATE_COMMENT = "comment/updateComment";
 const DELETE_COMMENT = "comment/deleteComment";
 
-const getComments = (comment) => ({
+const getComments = (comments) => ({
   type: GET_COMMENTS,
-  payload: comment
+  comments,
 });
 
 const postComment = (expenseId, comment) => ({
   type: POST_COMMENT,
-  payload: { expenseId, comment },
+  expenseId,
+  comment,
 });
 
 const updateComment = (commentId, comment) => ({
   type: UPDATE_COMMENT,
-  payload: { commentId, comment },
+  commentId,
+  comment,
 });
 
 const deleteComment = (commentId) => ({
   type: DELETE_COMMENT,
-  payload: commentId,
+  commentId,
 });
 
 export const thunkGetComments = (expenseId) => async (dispatch) => {
-	const response = await fetch(`/api/expenses/${expenseId}/comments`);
+  const response = await fetch(`/api/expenses/${expenseId}/comments`);
 
   if (response.ok) {
     const data = await response.json();
     dispatch(getComments(data));
   } else if (response.status < 500) {
     const errorMessages = await response.json();
-    return errorMessages
+    return errorMessages;
   } else {
-    return { server: "Something went wrong. Please try again" }
+    return { server: "Something went wrong. Please try again" };
   }
 };
 
 export const thunkPostComment = (expenseId, comment) => async (dispatch) => {
   const response = await fetch(`/api/expenses/${expenseId}/comments`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ comment })
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ comment }),
   });
 
   if (response.ok) {
@@ -57,9 +59,9 @@ export const thunkPostComment = (expenseId, comment) => async (dispatch) => {
 
 export const thunkUpdateComment = (commentId, comment) => async (dispatch) => {
   const response = await fetch(`/api/comments/${commentId}`, {
-    method: 'UPDATE',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ comment })
+    method: "UPDATE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ comment }),
   });
 
   if (response.ok) {
@@ -78,16 +80,17 @@ const initialState = { comments: null };
 function commentReducer(state = initialState, action) {
   switch (action.type) {
     case GET_COMMENTS:
-      return { ...state, comments: action.payload };
+      return { ...state, comments: action.comments };
     case POST_COMMENT:
-      return { ...state, comments: [...state?.comments, action.payload] };
+      return { ...state, comments: [...state?.comments, action.comment] };
     case UPDATE_COMMENT:
-      const updatedComments = state.comments.map(comment =>
-        comment.id === action.payload.id ? action.payload : comment
+      const updatedComments = state.comments.map((comment) =>
+        comment.id === action.commentId ? action.comment : comment
       );
       return { ...state, comments: updatedComments };
     case DELETE_COMMENT:
-      return { ...state, comments: action.payload };
+      return state;
+    // return { ...state, comments: action.payload };
     default:
       return state;
   }
