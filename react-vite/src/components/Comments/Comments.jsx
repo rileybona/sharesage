0a455eb80283db336053d80422e417f3f30d1a15
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-05 11:24:19 -0500

    modal displays for delete comment

--- react-vite/src/components/Comments/Comments.jsx
+++ react-vite/src/components/Comments/Comments.jsx
@@ -1,3 +1,6 @@
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import SubmitCommentModal from "../CommentModals/SubmitCommentModal";
+import DeleteCommentModal from "../CommentModals/DeleteCommentModal";
 import { useEffect, useState } from "react";
 import {
   thunkGetComments,
@@ -14,42 +17,57 @@ function Comments() {
   const { expenseId } = useParams();
   const userComments = useSelector((state) => state?.comment?.comments);
   const userSession = useSelector((state) => state?.session?.user);
-  const [userCommented, setUserCommented] = useState()
+  const [userCommented, setUserCommented] = useState();
 
   useEffect(() => {
     dispatch(thunkGetComments(expenseId));
   }, [dispatch, expenseId]);
 
   useEffect(() => {
-    setUserCommented(userComments?.find(c => c.user_id == userSession.id))
-  }, [userComments])
+    setUserCommented(userComments?.find((c) => c.user_id == userSession.id));
+  }, [userComments]);
 
-  function handlePost(expenseId) {
-    dispatch(thunkPostComment(expenseId, "This is my test template POST comment!"))
-    console.log('my post posted', expenseId)
-  }
+  // function handlePost(expenseId, commentText) {
+  //   dispatch(thunkPostComment(expenseId, commentText));
+  //   console.log("my post posted", expenseId);
+  // }
 
-  function handleUpdate(commentId) {
-    dispatch(thunkUpdateComment(commentId, "This is my test template UPDATE comment!"))
-    console.log('my post updated', commentId)
-  }
+  // function handleUpdate(commentId, commentText) {
+  //   dispatch(thunkUpdateComment(commentId, commentText));
+  //   console.log("my post updated", commentId);
+  // }
 
-  function handleDelete(commentId) {
-    dispatch(thunkDeleteComment(commentId))
-  }
+  // function handleDelete(commentId) {
+  //   dispatch(thunkDeleteComment(commentId));
+  // }
 
   return (
     <div id="comments">
       <h1>Comments</h1>
-      { !userCommented && <button onClick={() => handlePost(expenseId)}>Post your comment</button> }
+      {!userCommented && (
+        <OpenModalButton
+          className="post-comment-button"
+          buttonText="Post your comment"
+          modalComponent={<SubmitCommentModal props={expenseId} />}
+        />
+      )}
       {userComments?.map((comment, i) => (
         <div className="comment" key={i}>
-          <div className="comment-header">Created {comment?.created_at?.slice(0, -13)} by {comment?.user?.first_name}</div>
+          <div className="comment-header">
+            Created {comment?.created_at?.slice(0, -13)} by{" "}
+            {comment?.user?.first_name}
+          </div>
           <div className="comment-body">{comment?.text}</div>
-          { comment.user_id == userSession.id && (
+          {comment.user_id == userSession.id && (
             <div className="comment-buttons">
-              <button onClick={() => handleUpdate(comment?.id)}>Update</button>
-              <button onClick={() => handleDelete(comment?.id)}>Delete</button>
+              <OpenModalButton
+                buttonText="Update"
+                modalComponent={<SubmitCommentModal props={comment?.user_id} />}
+              />
+              <OpenModalButton
+                buttonText="Delete"
+                modalComponent={<DeleteCommentModal props={comment?.user_id} />}
+              />
             </div>
           )}
         </div>
