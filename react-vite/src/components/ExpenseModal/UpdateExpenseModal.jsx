f5e3f8aff8f3f9b3f8355e3f611cd0900aa60214
David Liu <davidas99017@gmail.com>
2024-07-05 17:48:07 -0400

    update expense modal

--- react-vite/src/components/ExpenseModal/UpdateExpenseModal.jsx
+++ react-vite/src/components/ExpenseModal/UpdateExpenseModal.jsx
@@ -1,47 +1,134 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
-import { getListOfPayees, updateAnExpense } from "../../redux/expense";
+import {
+  addExpensePayees,
+  getExpensePayees,
+  getListOfPayees,
+  updateAnExpense,
+} from "../../redux/expense";
 import Select from "react-select";
+import makeAnimated from "react-select/animated";
 
 const EXPENSE_TYPE = ["Other", "Food", "Travel", "Utilites"];
 
 export default function UpdateExpenseModal({ expenseId, setReload, reload }) {
   const sessionUser = useSelector((state) => state.session.user);
+  //for prepopulating the form
   const curExpense = useSelector(
     (state) => state.expense.expense_details[expenseId]
   );
+  //all users object from redux
   const userList = useSelector((state) => state.expense.payees);
+  // existing payees associated with this root expense
+  const existingPayees = useSelector((state) => state.expense.expense_details)[
+    expenseId
+  ].payees;
+  //select options are constructed with the all-user list
+  let selectOptions;
+  //existing  payee emails parsed from existingPayees;
+  let ogPayees;
+  //default options gets constructed once existing payees are loaded;
+  //default options is passed into the Select component in React DOM
+  //for prepopulation;
+  let defaultOptions;
 
   //parsing transaction date to locale date string for field population
   const transaction_date = new Date(
     curExpense.transaction_date
   ).toLocaleDateString("en-CA");
+  //to include react select animations
+  const animatedComponents = makeAnimated();
 
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+
   const [name, setName] = useState(curExpense.name);
   const [amount, setAmount] = useState(parseFloat(curExpense.amount));
   const [type, setType] = useState(curExpense.type);
   const [date, setDate] = useState(transaction_date);
+  //state to reload component when users and existing payees gets fetched from the store
   const [userLoaded, setUserLoaded] = useState(false);
+  const [existingPayeesLoaded, setExistingPayeesLoaded] = useState(false);
+  //state to store selected users within the react select component
+  const [selectedUsers, setSelectedUsers] = useState([]);
 
-  console.log(userList);
+  const constructSelectOptions = (userList, payees) => {
+    const initialOptions = [];
+    const allOptions = Object.keys(userList).length
+      ? [...Object.values(userList)].reduce((acc, user) => {
+          const option = {
+            value: user.email,
+            label: `${user.first_name} ${user.last_name}`,
+          };
+          acc.push(option);
+          if (payees.includes(user.email)) initialOptions.push(option);
+          return acc;
+        }, [])
+      : [];
+    return [allOptions, initialOptions];
+  };
 
-  // const constructSelectOptions = (userList) => {
-  //   return Object.values(userList).reduce((acc, el) => {
-  //     const option = {value: }
-  //   }, [])
-  // };
+  const getExistingEmails = (usersObj) => {
+    return Object.keys(usersObj).length
+      ? Object.values(usersObj).reduce((acc, user) => {
+          acc.push(user.email);
+          return acc;
+        }, [])
+      : [];
+  };
 
   useEffect(() => {
-    dispatch(getListOfPayees()).then(setUserLoaded(true));
-  }, [dispatch]);
+    dispatch(getListOfPayees()).then(() => setUserLoaded(true));
+    dispatch(getExpensePayees(expenseId)).then(() =>
+      setExistingPayeesLoaded(true)
+    );
+  }, [dispatch, expenseId]);
+
+  if (userLoaded && existingPayeesLoaded) {
+    ogPayees = getExistingEmails(existingPayees);
+    [selectOptions, defaultOptions] = constructSelectOptions(
+      userList,
+      ogPayees
+    );
+  }
 
   if (!curExpense) return <h2>Something went wrong. </h2>;
 
+  const handleChange = (e) => {
+    setSelectedUsers(e.map((e) => e.value));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const payeeCount = selectedUsers.length
+      ? selectedUsers.length
+      : defaultOptions.length;
+
+    const split_amount = amount / payeeCount;
+    const existingEmails = defaultOptions.map((e) => e.value);
+
+    const childExpensePayload = {
+      existing_payees: defaultOptions.reduce((acc, el) => {
+        if (selectedUsers.includes(el.value)) {
+          acc.push({
+            email: el.value,
+            split_amount,
+          });
+        }
+        return acc;
+      }, []),
+      new_payees: selectedUsers.reduce((acc, el) => {
+        if (!existingEmails.includes(el)) {
+          acc.push({
+            email: el,
+            split_amount,
+          });
+        }
+        return acc;
+      }, []),
+    };
+
     const newExpense = {
       owner_id: sessionUser.id,
       name,
@@ -60,7 +147,8 @@ export default function UpdateExpenseModal({ expenseId, setReload, reload }) {
         newDate.getFullYear();
       newExpense.transaction_date = formatDate;
     }
-    dispatch(updateAnExpense(expenseId, newExpense))
+    dispatch(updateAnExpense(expenseId, newExpense));
+    dispatch(addExpensePayees(expenseId, childExpensePayload))
       .then(() => {
         setReload(reload + 1);
       })
@@ -68,10 +156,23 @@ export default function UpdateExpenseModal({ expenseId, setReload, reload }) {
   };
 
   //short curcuiting component if user list not loaded
-  if (!userLoaded) return <h2>loading</h2>;
+  if (!userLoaded || !existingPayeesLoaded) return <h2>loading</h2>;
   return (
     <form onSubmit={handleSubmit}>
-      <h1>Add an expense</h1>
+      <h1>Update expense</h1>
+      <Select
+        closeMenuOnSelect={true}
+        components={animatedComponents}
+        isMulti
+        onChange={(e) => {
+          handleChange(e);
+          // console.log(e);
+        }}
+        options={selectOptions}
+        defaultValue={(() => {
+          return defaultOptions;
+        })()}
+      />
       <label>
         <input
           type="text"
