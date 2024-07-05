b7d2fffc511678224078d21f24ad5e82de6f0889
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-05 12:12:53 -0500

    now comments can update

--- react-vite/src/components/CommentModals/UpdateCommentModal.jsx
+++ react-vite/src/components/CommentModals/UpdateCommentModal.jsx
@@ -1,21 +1,27 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { useModal } from "../../context/Modal";
 import { thunkUpdateComment } from "../../redux/comment";
 import "./CommentModal.css";
 
 export default function UpdateCommentModal({ commentId }) {
   const { closeModal } = useModal();
+  const [comment, setComment] = useState();
   const dispatch = useDispatch();
 
   function handleUpdate() {
-    dispatch(thunkUpdateComment(commentId, "testing update comment"));
+    dispatch(thunkUpdateComment(commentId, comment));
     closeModal();
   }
 
   return (
     <div id="comment-modal">
       <h1>Update your comment?</h1>
-      <textarea name="post-comment-body" id="post-comment-area" />
+      <textarea
+        name="post-comment-body"
+        id="post-comment-area"
+        onChange={(e) => setComment(e.target.value)}
+      />
       <div className="comment-modal-buttons">
         <button className="modal-button" onClick={handleUpdate}>
           UPDATE
