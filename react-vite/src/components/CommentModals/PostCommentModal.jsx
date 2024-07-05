5923afe8351f489bf0033de058572afa13925065
Timothy Macfarlane <timdiscovers@gmail.com>
2024-07-05 12:11:51 -0500

    you can now post a custom comment

--- react-vite/src/components/CommentModals/PostCommentModal.jsx
+++ react-vite/src/components/CommentModals/PostCommentModal.jsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { useModal } from "../../context/Modal";
 import { thunkPostComment } from "../../redux/comment";
 import "./CommentModal.css";
@@ -6,16 +7,21 @@ import "./CommentModal.css";
 export default function PostCommentModal({ expenseId }) {
   const { closeModal } = useModal();
   const dispatch = useDispatch();
+  const [comment, setComment] = useState();
 
   function handlePost() {
-    dispatch(thunkPostComment(expenseId, "testing post comment"));
+    dispatch(thunkPostComment(expenseId, comment));
     closeModal();
   }
 
   return (
     <div id="comment-modal">
       <h1>Post your comment!</h1>
-      <textarea name="post-comment-body" id="post-comment-area" />
+      <textarea
+        name="post-comment-body"
+        id="post-comment-area"
+        onChange={(e) => setComment(e.target.value)}
+      />
       <div className="comment-modal-buttons">
         <button className="modal-button" onClick={handlePost}>
           POST
