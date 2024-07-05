f189aeb1f80926cb6f7f5310d131646501e2dc27
Riley Bona <rileybona7@gmail.com>
2024-07-05 09:38:40 -0400

    begin recent activity

--- react-vite/src/components/RecentActivityPage/RecentActivityPage.jsx
+++ react-vite/src/components/RecentActivityPage/RecentActivityPage.jsx
@@ -1,16 +1,49 @@
 import './RecentActivityPage.css'
-
+import { useDispatch, useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getPayments } from '../../redux/payment'
+import { getAllExpenses } from '../../redux/expense'
 
 function RecentActivityPage() {
+    const dispatch = useDispatch();
+    const [myDone, setMyDone] = useState(false);
+    const [allDone, setAllDone] = useState(false);
+    const [myPayments, setMyPayments] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+
+    const paymentState = useSelector(state => state.payment); 
+    const expenseState = useSelector(state => state.expense.root_expenses);
+    const userState = 1;
+
+
+    useEffect(() => {
+        dispatch(getPayments()); 
+        dispatch(getAllExpenses());
+    }, [dispatch]); 
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (Object.keys(expenseState).length > 0 && Object.keys(paymentState).length > 0) {
+                setMyPayments(Object.values(paymentState));
+                setExpenses(Object.values(expenseState));
+                setMyDone(true);
+            }
+        }, 20);
+    },[paymentState]);
+
 
     return (
         <>
-            <h1>Recent Activity</h1>
-            <ul>
-                <li>phillip paid u</li>
-                <li>david updated Lunch</li>
-                <li>tim defaulted on his loan</li>
-            </ul>
+            {!myDone ? <p>not loaded or maybe u dont have any payments</p> : 
+                <div className='out-div-payments'>
+                    <ul>
+                        {myPayments.map((payment) => {
+                            <li className='mypayment-li'>{``}</li>
+                        })} 
+                    </ul>
+                </div>
+            }
         </>
     )
 }
