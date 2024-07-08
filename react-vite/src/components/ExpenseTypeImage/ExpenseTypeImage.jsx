12611ad2bd7ed29c755bccfc6e5b1151594f5d08
David Liu <davidas99017@gmail.com>
2024-07-08 01:31:45 -0400

    adding static references

--- react-vite/src/components/ExpenseTypeImage/ExpenseTypeImage.jsx
+++ react-vite/src/components/ExpenseTypeImage/ExpenseTypeImage.jsx
@@ -1,11 +1,17 @@
+import food from "/public/food.png";
+import travel from "/public/travel.png";
+import home from "/public/home.png";
+import entertainment from "/public/entertainment.png";
+import other from "/public/other.png";
+
 export default function ExpenseTypeImage({ type, allExpenses = false }) {
   const types_url = new Map();
-    types_url.set("Food", "../../public/food.png");
-    types_url.set("Travel", "../../public/travel.png");
-    types_url.set("Home", "../../public/home.png");
-    types_url.set("Entertainment", "../../public/entertainment.png");
-    types_url.set("Other", "../../public/other.png");
-    types_url.default = "https://dummyimage.com/300";
+  types_url.set("Food", food);
+  types_url.set("Travel", travel);
+  types_url.set("Home", home);
+  types_url.set("Entertainment", entertainment);
+  types_url.set("Other", other);
+  types_url.default = "https://dummyimage.com/300";
 
   const imgsrc = types_url.get(type) || types_url.default;
   return (
