5a3556cc0ec461eb5ed647fd9b6e3b772dd073b9
Riley Bona <rileybona7@gmail.com>
2024-08-06 14:53:06 -0400

    adjust childexpense seeders

--- app/seeds/child_expenses.py
+++ app/seeds/child_expenses.py
@@ -6,15 +6,15 @@ from alembic import op
 # Adds a demo user, you can add other users here if you want
 def seed_child_expenses():
 
-    dinner_expense1 = ChildExpense(root_expense_id=1, user_id=2, split_amount=83.34)
+    dinner_expense1 = ChildExpense(root_expense_id=1, user_id=2, split_amount=83.34, balance=83.34)
 
     dinner_expense2 = ChildExpense(root_expense_id=1, user_id=3, split_amount=83.34)
 
-    grocery_expense = ChildExpense(root_expense_id=2, user_id=2, split_amount=40)
+    grocery_expense = ChildExpense(root_expense_id=2, user_id=2, split_amount=40, balance=66.67)
 
     travel_expense1 = ChildExpense(root_expense_id=3, user_id=1, split_amount=266.67)
 
-    travel_expense2 = ChildExpense(root_expense_id=3, user_id=3, split_amount=266.67)
+    travel_expense2 = ChildExpense(root_expense_id=3, user_id=3, split_amount=266.67, balance=66.67)
 
     db.session.add(dinner_expense1)
     db.session.add(dinner_expense2)
