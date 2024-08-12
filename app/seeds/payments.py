83581b00ed69a735767df9ce7c8ab09989e8918a
David Liu <davidas99017@gmail.com>
2024-08-12 16:57:32 -0400

    updated seeding

--- app/seeds/payments.py
+++ app/seeds/payments.py
@@ -4,33 +4,117 @@ from alembic import op
 
 
 def seed_payments():
-
-    payment1 = Payment(
+    food0_marnie_to_demo = Payment(
         user_id=2,
         expense_id=1,
         root_expense_id=1,
-        recipient_id = 1,
+        recipient_id=1,
         method="venmo",
         amount=83.34,
         note="I can cover the rest if need be",
         created_at=db.func.now(),
     )
-    payment2 = Payment(
-        user_id=3, expense_id=3, root_expense_id=2, recipient_id=1, method="cash", amount=66.67, created_at=db.func.now()
-    )
-    payment3 = Payment(
+
+    food0_bobbie_to_demo = Payment(
         user_id=3,
-        expense_id=5, 
-        root_expense_id=3,
-        recipient_id=2,
-        method="paypal",
-        amount=66.67,
+        expense_id=1,
+        root_expense_id=1,
+        recipient_id=1,
+        method="cash",
+        amount=83.34,
+        note="paying da bill",
         created_at=db.func.now(),
     )
 
-    db.session.add(payment1)
-    db.session.add(payment2)
-    db.session.add(payment3)
+    # food1_demo_to_marnie1 = Payment(
+    #     user_id=1,
+    #     expense_id=2,
+    #     root_expense_id=2,
+    #     recipient_id=2,
+    #     method="paypal",
+    #     amount=20,
+    #     note="covering half",
+    #     created_at=db.func.now(),
+    # )
+
+    # food1_demo_to_marnie2 = Payment(
+    #     user_id=1,
+    #     expense_id=2,
+    #     root_expense_id=2,
+    #     recipient_id=2,
+    #     method="cash",
+    #     amount=20,
+    #     note="covering the rest in cash",
+    #     created_at=db.func.now(),
+    # )
+
+    food1_bobbie_to_marnie = Payment(
+        user_id=3,
+        expense_id=2,
+        root_expense_id=2,
+        recipient_id=2,
+        method="venmo",
+        amount=40,
+        note="I gotchu",
+        created_at=db.func.now(),
+    )
+
+    food2_demo_to_marnie = Payment(
+        user_id=1,
+        expense_id=5,
+        root_expense_id=3,
+        recipient_id=2,
+        method="cash",
+        amount=10,
+        note="for the coffee",
+        created_at=db.func.now(),
+    )
+
+    home0_bobbie_to_demo = Payment(
+        user_id=3,
+        expense_id=6,
+        root_expense_id=4,
+        recipient_id=1,
+        method="venmo",
+        amount=40,
+        note="beer money",
+        created_at=db.func.now(),
+    )
+
+    home1_demo_to_marnie = Payment(
+        user_id=1,
+        expense_id=7,
+        root_expense_id=5,
+        recipient_id=2,
+        method="cash",
+        amount=10,
+        created_at=db.func.now(),
+    )
+
+    home2_marnie_to_demo = Payment(
+        user_id=2,
+        expense_id=8,
+        root_expense_id=6,
+        recipient_id=1,
+        method="paypal",
+        amount=20,
+        note="for the paint",
+        created_at=db.func.now(),
+    )
+
+    for payment in [
+        food0_marnie_to_demo,
+        food0_bobbie_to_demo,
+        # food1_demo_to_marnie1,
+        # food1_demo_to_marnie2,
+        food1_bobbie_to_marnie,
+        food2_demo_to_marnie,
+        home0_bobbie_to_demo,
+        home1_demo_to_marnie,
+        home2_marnie_to_demo,
+    ]:
+        db.session.add(payment)
+
     db.session.commit()
 
 
--- app/seeds/root_expenses.py
+++ app/seeds/root_expenses.py
@@ -4,17 +4,46 @@ from alembic import op
 
 
 def seed_root_expenses():
+    # types : entertainment, food, home, travel, other
 
-    small_expense = RootExpense(
+    food0 = RootExpense(  # Demo (1)
         owner_id=1, name="Dinner", amount="250", expense_type="Food"
     )
 
-    smaller_expense = RootExpense(
-        owner_id=1, name="Groceries", amount="80", expense_type="Food"
+    food1 = RootExpense(  # marnie (2)
+        owner_id=2, name="Popeye's", amount="120", expense_type="Food"
     )
 
-    big_expense = RootExpense(
+    food2 = RootExpense(  # marnie (3)
+        owner_id=2, name="Starbucks", amount="20", expense_type="Food"
+    )
+
+    home0 = RootExpense(  # Demo (4)
+        owner_id=1, name="Groceries", amount="80", expense_type="Home"
+    )
+
+    home1 = RootExpense(  # marnie (5)
+        owner_id=2, name="Coffee pods", amount="20", expense_type="Home"
+    )
+
+    home2 = RootExpense(  # Demo (6)
+        owner_id=1, name="Paint Supplies", amount="40", expense_type="Home"
+    )
+
+    travel0 = RootExpense(  # marnie (7)
         owner_id=2, name="AirBnb", amount="800", expense_type="Travel"
+    )
+
+    travel1 = RootExpense(  # bobbie (8)
+        owner_id=3, name="Roadtrip Gas", amount="80", expense_type="Travel"
+    )
+
+    travel2 = RootExpense(  # Demo (9)
+        owner_id=1, name="Plane tickets", amount="560", expense_type="Travel"
+    )
+
+    other0 = RootExpense(  # Demo (10)
+        owner_id=1, name="Netflix subscription", amount="15", expense_type="Other"
     )
 
     # expense1 = RootExpense(
@@ -38,10 +67,19 @@ def seed_root_expenses():
     # expenses = [expense1, expense2, expense3]
     # for exp in expenses:
     #     db.session.add(exp)
-
-    db.session.add(small_expense)
-    db.session.add(smaller_expense)
-    db.session.add(big_expense)
+    for expense in [
+        food0,
+        food1,
+        food2,
+        home0,
+        home1,
+        home2,
+        travel0,
+        travel1,
+        travel2,
+        other0,
+    ]:
+        db.session.add(expense)
     db.session.commit()
 
 
