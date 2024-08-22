42d368b3240a7a024f1c1c3d675c947ecf60de28
philliam-fancyson <philn0415@gmail.com>
2024-08-21 20:26:15 -0700

    landing-page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,20 @@
 
 export default function LandingPage() {
     return (
-        <>
-            <h1>Test</h1>
+        <div className="flex flex-col items-center w-full h-full">
+            <div className="flex w-full justify-center">
+                <h1 className="flex flex-col">
+                    <span className="text-xs">
+                        Share more. Stress Less.
+                    </span>
+                    <span>
+                        Words Here Lorem Ipsum
+                </span>
+                </h1>
+                <img src="/other.png" alt="Sage" className="w-2/5"/>
+            </div>
+            <button>Sign Up</button>
+        </div>
 
-        </>
     )
 }
